a0ea48bd000f72f45c41b8740d4369f654df3399
spyder-griffith <griffith.thomas@spyderbat.com>
2024-08-12 16:58:45 -0500

    Update Falco integration scripts to work properly
    * ask the user if they want Falco
    * use the correct Falco configuration for Spyderbat

--- install.sh
+++ install.sh
@@ -12,49 +12,54 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-if [ -x "$(command -v jq)" ]; then
-  if [ -x "$(command -v spyctl)" ]; then
-    SPYCTL_CONTEXT=$(spyctl config current-context)
-
-    if [ $? -ne 0 ]; then
-      echo "No current spyctl context found. Please configure spyctl with access to the Spyderbat organization that the cluster is on."
-      exit 1
-    fi
-  fi
-fi
-
 cat << EOF
 == Spyderbat Evaluation Samples Installer ==
 
 Installing into the kubectl context: $CONTEXT
 EOF
 
-if [ ! ( -x "$(command -v jq)" && -x "$(command -v spyctl)" ) ]; then
-  echo -n "Please enter the Spyderbat organization ID for this cluster: "
-  read SPYDERBAT_ORG
-  echo -n "Please enter a valid Spyderbat API key for this organization: "
-  read SPYDERBAT_API_KEY
-else
-  echo "Installing into the spyctl context: $SPYCTL_CONTEXT"
-  SPYDERBAT_ORG=$(spyctl config view -o json | jq -r '.contexts | select(.[].name="'$(spyctl config current-context)'") | .[0].context.organization')
-  SPYCTL_SECRET=$(spyctl config view -o json | jq -r '.contexts | select(.[].name="'$(spyctl config current-context)'") | .[0].secret')
-  SPYDERBAT_API_KEY=$(spyctl config get-apisecrets "$SPYCTL_SECRET" -o json | jq -r .stringData.apikey)
-fi
+echo -n "Would you like to install the Falco integration? [Y/n]: "
+read USE_FALCO
 
-helm list -n falco | grep falco > /dev/null
-if $?; then
-  OPERATION=install
-else
-  OPERATION=update
-fi
+if [ ! ( ${USE_FALCO:0:1} == "n" || ${USE_FALCO:0:1} == "N" ) ]; then
+  if [ ${USE_FALCO:0:1} == "y" || ${USE_FALCO:0:1} == "Y" || ${USE_FALCO:0:1} == "" ]; then
 
-helm $OPERATION falco . \
-  --create-namespace \
-  --namespace falco \
-  --set falcosidekick.enabled=true \
-  --set falcosidekick.config.spyderbat.orguid="$SPYDERBAT_ORG" \
-  --set falcosidekick.config.spyderbat.apiurl="${SPYDERBAT_API_URL:-https://api.spyderbat.com}" \
-  --set falcosidekick.config.spyderbat.apikey="$SPYDERBAT_API_KEY"
+    if [ -x "$(command -v jq)" && -x "$(command -v spyctl)" ]; then
+      SPYCTL_CONTEXT=$(spyctl config current-context)
+
+      if [ $? -ne 0 ]; then
+        echo "No current spyctl context found. Please configure spyctl with access to the Spyderbat organization that the cluster is on."
+        exit 1
+      fi
+
+      echo "Installing into the spyctl context: $SPYCTL_CONTEXT"
+      SPYDERBAT_ORG=$(spyctl config view -o json | jq -r '.contexts | select(.[].name="'$(spyctl config current-context)'") | .[0].context.organization')
+      SPYCTL_SECRET=$(spyctl config view -o json | jq -r '.contexts | select(.[].name="'$(spyctl config current-context)'") | .[0].secret')
+      SPYDERBAT_API_KEY=$(spyctl config get-apisecrets "$SPYCTL_SECRET" -o json | jq -r .stringData.apikey)
+    else
+      echo -n "Please enter the Spyderbat organization ID for this cluster: "
+      read SPYDERBAT_ORG
+      echo -n "Please enter a valid Spyderbat API key for this organization: "
+      read SPYDERBAT_API_KEY
+    fi
+
+    helm update falco falcosecurity/falco \
+      --create-namespace \
+      --namespace falco \
+      --set falcosidekick.enabled=true \
+      --set falcosidekick.config.spyderbat.orguid="$SPYDERBAT_ORG" \
+      --set falcosidekick.config.spyderbat.apiurl="${SPYDERBAT_API_URL:-https://api.spyderbat.com}" \
+      --set falcosidekick.config.spyderbat.apikey="$SPYDERBAT_API_KEY" \
+      --set extra.args=\{"-p","%proc.pid"\}
+
+  else
+    echo "Input not recognized; please enter y or n!"
+    echo "No services installed"
+    exit 1
+  fi
+else
+  echo "Skipping falco integration..."
+fi
 
 kubectl apply -R -f modules
 
